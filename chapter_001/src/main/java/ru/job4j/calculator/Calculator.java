c230f03cd226b1d476ce98dcc17b662414268c55
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-13 22:19:23 +0300

    JavaDoc. Code convention[#219850]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
+/**
+ * Class Calculator
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 13.01.2020
+ * @version 1
+ */
 public class Calculator {
     public static void add(double first, double second){
         double result = first + second;
@@ -25,6 +31,10 @@ public class Calculator {
         }
     }
 
+    /**
+     * Main - метод для тестирования арифметических операций
+     * @param args
+     */
     public static void main(String[] args) {
         add(1,1);
         div(4,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculator task
+ *
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+package ru.job4j.calculator;
