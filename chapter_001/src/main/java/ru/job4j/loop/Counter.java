18ff747a18030439cf714f8567a608814dd79493
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-13 10:28:15 +0300

    Подсчет суммы чётных чисел в диапазоне[#219847]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public int addUsingWhile(int start, int finish){
+        int i = start;
+        int total = 0;
+        while (i <= finish){
+            if (i % 2 == 0){
+                total+=i;
+            }
+            i++;
+        }
+        return total;
+    }
+
+    public int addUsingFor(int start, int finish){
+        int total = 0;
+        for (int i = start; i<=finish; i++){
+            if (i % 2 == 0)
+                total+=i;
+        }
+        return total;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFrom1To10Then30UsingWhile(){
+        Counter counter = new Counter();
+        int result = counter.addUsingWhile(1, 10);
+        assertThat(result, is(30));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFrom1To10Then30UsingFor(){
+        Counter counter = new Counter();
+        int result = counter.addUsingWhile(1, 10);
+        assertThat(result, is(30));
+    }
+
+    @Test
+    public void whenStartIsEqualToFinishAndOddUsingFor(){
+        Counter counter = new Counter();
+        int result = counter.addUsingWhile(1, 1);
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void whenStartAndFinishIs10UsingFor(){
+        Counter counter = new Counter();
+        int result = counter.addUsingWhile(10, 10);
+        assertThat(result, is(10));
+    }
+}
