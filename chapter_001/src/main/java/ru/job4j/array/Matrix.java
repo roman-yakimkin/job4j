6c4ea98b4b4fbd85dcb5e943a2a5e9b1c64e8804
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-15 15:21:51 +0300

    Двухмерный массив. Таблица умножения.[#219784]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Класс, предназначенный для заполнения двумерного массива произведениями индексов строк и столбцов
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 15.01.2020
+ * @version 1.0
+ */
+public class Matrix {
+
+    /**
+     * Формирование матрицы, содержащей произведения строк и столбцов
+     * @param size - размер матрицы
+     * @return table - матрица, содержащая произведения индексов строк и столбцов
+     */
+    public int[][] multiple(int size){
+        int[][] table = new int[size][size];
+
+        for (int col=0; col<size; col++){
+            for (int row = 0; row<size; row++){
+                table[col][row] = (col+1) * (row+1);
+            }
+        }
+        return table;
+    }
+}
