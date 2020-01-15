71b272e2f42f5ffdc00b9b3e68858363aacecb3a
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-15 17:04:56 +0300

    Моно столбец в матрице.[#219799]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,21 +1,36 @@
 package ru.job4j.array;
 
 /**
- * Класс, проверяющий моностроку в таблице
+ * Класс, проверяющий моностроку и моностолбец в таблице
  * @author Roman Yakimkin (r.yakimkin@yandex.ru)
  * @since 15.01.2020
- * @version 1.0
+ * @version 1.1
  */
 public class MatrixCheck {
 
     /**
      * Проверка на то, что все элементы определенной строки матрицы заполнены символами 'X'
-     * @param board - символьная матрива
+     * @param board - символьная матрица
      * @param row - индекс строки
      * @return истина, если все элементы заполнены символами 'X'
      */
     public static boolean monoHorizontal(char[][] board, int row){
         for(int col=0; col<board[row].length; col++){
+            if (board[row][col] != 'X'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Проверка на то, что все элементы определенного столбца матрицы заполнены символами 'X'
+     * @param board - символьная матрица
+     * @param col - индекс столбца
+     * @return истина, если все элементы заполнены символами 'X'
+     */
+    public static boolean monoVertical(char[][] board, int col){
+        for(int row=0; row<board.length; row++){
             if (board[row][col] != 'X'){
                 return false;
             }
