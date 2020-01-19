898181ccee4ae55a70bd8bcbb40ff6f17813e5ea
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-19 13:22:43 +0300

    Создание объекта[#219937]

---
+++ chapter_002/src/main/java/ru/job4/oop/Cat.java
@@ -0,0 +1,19 @@
+package ru.job4.oop;
+
+/**
+ * Учебный класс, представляющий кошку
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 19.01.2020
+ * @version 1.0
+ */
+public class Cat {
+
+    /**
+     * Статическая функция для запуска класса
+     * @param args - аргументы для запуска
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4/oop/Dog.java
@@ -0,0 +1,20 @@
+package ru.job4.oop;
+
+/**
+ * Учебный класс, представляющий собаку
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 19.01.2020
+ * @version 1.0
+ */
+public class Dog {
+
+    /**
+     * Статическая функция для запуска класса
+     * @param args - аргументы для запуска
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhuchka = new Dog();
+    }
+}
