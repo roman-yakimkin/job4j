2eec9e1680f0f54a9ecf09cb37f56411667607e2
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-20 19:40:33 +0300

    Состояние объекта.[#219943]

---
+++ chapter_002/src/main/java/ru/job4/oop/Battery.java
@@ -0,0 +1,45 @@
+package ru.job4.oop;
+
+/**
+ * Класс, представляющий батарею
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 20.01.2020
+ * @version 1.0
+ */
+public class Battery {
+
+    /**
+     * Заряд батареи
+     */
+    private int load;
+
+    /**
+     * Конструктор батареи
+     * @param load - первоначальное значение заряда
+     */
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    /**
+     * Передать заряд другой батарее
+     * @param another
+     */
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    /**
+     * Статическая функция для запуска класса
+     * @param args - аргументы для запуска
+     */
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4/oop/Jar.java
@@ -0,0 +1,44 @@
+package ru.job4.oop;
+
+/**
+ * Класс, представляющий сосуд
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 20.01.2020
+ * @version 1.0
+ */
+public class Jar {
+
+    /**
+     * Количество воды в сосуде
+     */
+    public int value;
+
+    /**
+     * Конструктор сосуда
+     * @param size - количество воды
+     */
+    public Jar(int size) {
+        this.value = size;
+    }
+
+    /**
+     * Перелить с сосуд содержимое другого сосуда
+     * @param another - другой сосуд
+     */
+    public void pour(Jar another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+
+    /**
+     * Статическая функция для запуска класса
+     * @param args - аргументы для запуска
+     */
+    public static void main(String[] args) {
+        Jar first = new Jar(10);
+        Jar second = new Jar(5);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+        first.pour(second);
+        System.out.println("first : " + first.value + ". second : " + second.value);
+    }
+}
