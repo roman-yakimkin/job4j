488848b8b67650a0ef5b937521cec04533c2d93b
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-22 22:44:11 +0300

    Рефакторинг - Площадь треугольника.[#219934]

---
+++ chapter_002/src/main/java/ru/job4/oop/Triangle.java
@@ -0,0 +1,75 @@
+package ru.job4.oop;
+
+import ru.job4j.condition.Point;
+
+/**
+ * Класс, представляющий треугольник
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 15.01.2020
+ * @version 2.0
+ */
+public class Triangle {
+
+    /**
+     * Первая точка треугольника
+     */
+    private Point first;
+
+    /**
+     * Вторая точка треугольника
+     */
+    private Point second;
+
+    /**
+     * Третья точка треугольника
+     */
+    private Point third;
+
+    /**
+     * Конструктор треугольника
+     * @param ap - первая точка
+     * @param bp - вторая точка
+     * @param cp - третья точка
+     */
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Вычислить периметр треугольника
+     * @param a - длина первой стороны
+     * @param b - длина второй стороны
+     * @param c - длина третьей стороны
+     * @return - периметр треугольника
+     */
+    public static double perimeter(double a, double b, double c) {
+        return (a + b + c);
+    }
+
+    /**
+     * Вычисление полщади треугольника
+     * @return значение площади треугольника
+     */
+    public double area() {
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double semiPerimeter = perimeter(a, b, c) / 2;
+        double area = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+
+        return area;
+    }
+
+    /**
+     * Проверить, может ли существовать треугольник с данными сторонами
+     * @param ab - первая сторона
+     * @param ac - вторая сторона
+     * @param bc - третья сторона
+     * @return - истина, если треугольник существует
+     */
+    public static boolean exists(double ab, double ac, double bc) {
+        return (ab + ac > bc) && (ab + bc > ac) && (ac + bc > ab);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4/oop/TriangleTest.java
@@ -0,0 +1,39 @@
+package ru.job4.oop;
+
+import org.junit.Test;
+import ru.job4j.condition.Point;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTest {
+
+    @Test
+    public void whenExists() {
+        boolean result = Triangle.exists(3, 3, 5);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDoesNotExist() {
+        boolean result = Triangle.exists(6, 6, 15);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void areaIsSix() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(4, 0), new Point(4, 3));
+        double area = triangle.area();
+        double expected = 6.0;
+        assertEquals(area, expected, 0.01);
+    }
+
+    @Test
+    public void areaIsHalf() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(1, 0), new Point(0, 1));
+        double area = triangle.area();
+        double expected = 0.5;
+        assertEquals(area, expected, 0.01);
+    }
+}
