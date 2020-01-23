f243a69b16db35414ae17a24474a475add8b839a
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-23 09:06:43 +0300

    Расстояние между точками в трехмерном пространстве.[#219935]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,7 +4,7 @@ package ru.job4j.condition;
  * Класс, представляющий точку
  * @author Roman Yakimkin (r.yakimkin@yandex.ru)
  * @since 15.01.2020
- * @version 2.0
+ * @version 3.0
  */
 public class Point {
 
@@ -19,7 +19,12 @@ public class Point {
     private int y;
 
     /**
-     * Конструктор точки
+     *  Координата z точки
+     */
+    private int z;
+
+    /**
+     * Конструктор точки для двух координат
      * @param first - координата X
      * @param second - координата Y
      */
@@ -29,12 +34,33 @@ public class Point {
     }
 
     /**
+     * Конструктор точки для трех координат
+     * @param first
+     * @param second
+     * @param third
+     */
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
+    /**
      * Вычислить расстояние между двумя точками
      * @param that - вторая точка
-     * @return - расстояние меду двумя точками
+     * @return - расстояние между двумя точками
      */
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+
+    /**
+     * Вычислить трехмерное расстояние между двумя точками
+     * @param that - вторая точка
+     * @return - расстояние между двумя точками в трехмерном пространстве
+     */
+    public double distance3D(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2) + Math.pow(this.z - that.z, 2));
     }
 
     /**
@@ -42,9 +68,9 @@ public class Point {
      * @param args - аргументы для запуска
      */
     public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double dist = a.distance3D(b);
         System.out.println(dist);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -31,4 +31,33 @@ public class PointTest {
         double out = a.distance(b);
         Assert.assertNotEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void distance3D1() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(1, 1, 1);
+        double expected = Math.sqrt(3);
+        double out = a.distance3D(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distance3D2() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 0);
+        double expected = 0.0;
+        double out = a.distance3D(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distance3D3() {
+        Point a = new Point(10, 10, 10);
+        Point b = new Point(20, 10, 10);
+        double expected = 10.0;
+        double out = a.distance3D(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+
 }
