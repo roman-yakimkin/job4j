e3c00abf524712b856ec270b3f862ab21ec083db
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-01-27 13:11:54 +0300

    Массивы и модели.[#219956] - доработка кода

--- chapter_002/src/main/java/ru/job4j/pojo/Library.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Library.java
@@ -15,8 +15,8 @@ public class Library {
     public static void main(String[] args) {
         Book book1 = new Book("The war and peace", 1500);
         Book book2 = new Book("Eugene Onegin", 200);
-        Book book3 = new Book("The magical tales", 400);
-        Book book4 = new Book("Don Quixote", 300);
+        Book book3 = new Book("Clean code", 650);
+        Book book4 = new Book("Don Quixote", 350);
 
         Book[] books = new Book[4];
         books[0] = book1;
@@ -24,20 +24,25 @@ public class Library {
         books[2] = book3;
         books[3] = book4;
 
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        for (Book book : books) {
             System.out.println(book.getName() + " contains " + book.getPagesCount() + " pages");
         }
 
-        Book cleanCode = new Book("Clean code", 650);
-
+        Book bookTmp = books[3];
         books[3] = books[0];
-        books[0] = cleanCode;
+        books[0] = bookTmp;
 
-        System.out.println("After moving of the books");
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        System.out.println("After moving of the books =====");
+        for (Book book : books) {
             System.out.println(book.getName() + " contains " + book.getPagesCount() + " pages");
         }
+        System.out.println("Now we're trying to filter out and display all the books having name \"Clean code\" =====");
+        for (int i = 0; i < books.length; i++) {
+            Book book = books[i];
+            if (book.getName().equals("Clean code")) {
+                System.out.println("A book indexed " + i + " with name " + book.getName() + " contains " + book.getPagesCount() + " pages");
+            }
+        }
+
     }
 }
