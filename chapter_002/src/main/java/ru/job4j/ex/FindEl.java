02560aa46c3e46cd268b38d465acc1e23c942c09
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-05 09:29:01 +0300

    Пользовательские исключения.[#219929]

---
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,40 @@
+package ru.job4j.ex;
+
+/**
+ * Класс для поиска элемента в строке
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 05.02.2020
+ * @version 1.0
+ */
+public class FindEl {
+
+    /**
+     * Найти индекс элемента в массиве строк по ключу
+     * @param value - массив строк
+     * @param key - строка - ключ
+     * @return - индекс найденного элемента
+     * @throws ElementNotFoundException - исключение, если элемент не найден
+     */
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"first", "second", "third", "fourth", "fifth"};
+        try {
+            int index = indexOf(words, "sixth");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
