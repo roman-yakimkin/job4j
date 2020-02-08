69e751dd412de6d2c2609c7e97b577089ccef964
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-08 19:51:37 +0300

    Контрольные вопросы.[#219965]

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,38 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+/**
+ * Механизм возврата денег в лендиноговых аппаратах
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 08.02.2020
+ * @version 1.0
+ */
+public class Machine {
+    /**
+     * Разменные монеты
+     */
+    private final int[] COINS = {10, 5, 2, 1};
+
+    /**
+     * Вернуть сдачу монетами
+     * @param money - вносимая сумма в аппарат
+     * @param price - цена товара в аппарате
+     * @return - массив возвращаемых монет
+     */
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int coinIndex = 0;
+        int rest = money - price;
+        while (rest > 0) {
+            if (rest >= COINS[coinIndex]) {
+                rsl[size++] = COINS[coinIndex];
+                rest -= COINS[coinIndex];
+            } else {
+                coinIndex++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
