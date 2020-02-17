9007922b9c3f3a712b8ff2ec9d84547a07ad7e68
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-17 22:01:20 +0300

    Установлена сортировка по убыванию в TreeSort

--- chapter_003/src/main/java/ru/job4j/collection/TreeSort.java
+++ chapter_003/src/main/java/ru/job4j/collection/TreeSort.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -11,7 +12,7 @@ import java.util.TreeSet;
  */
 public class TreeSort {
     public static void main(String[] args) {
-        Set<Integer> numbers = new TreeSet<>();
+        Set<Integer> numbers = new TreeSet<>(Collections.reverseOrder());
         numbers.add(5);
         numbers.add(1);
         numbers.add(3);
