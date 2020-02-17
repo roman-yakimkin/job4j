06809e4ec786fb55beb11181660682bf6e062e8d
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-17 09:39:34 +0300

    Work in progress. I have to check out the Job_219885 branch

---
+++ chapter_003/src/main/java/ru/job4j/collection/Job.java
@@ -0,0 +1,39 @@
+package ru.job4j.collection;
+
+/**
+ * Класс - задача
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 16.01.2020
+ * @version 1.0
+ */
+public class Job {
+    /**
+     * Название задачи
+     */
+    private String name;
+    /**
+     * Приоритет задачи
+     */
+    private int priority;
+
+    public Job(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Job{"
+                + "name='" + name + '\''
+                + ", priority=" + priority
+                + '}';
+    }
+}
