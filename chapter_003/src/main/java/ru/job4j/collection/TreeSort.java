441377ddf5749d97327c20b8ac1a640e8345e39c
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-17 12:32:01 +0300

    Добавлен демонстрационный класс TreeSort

---
+++ chapter_003/src/main/java/ru/job4j/collection/TreeSort.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Класс для сортировки элементов множества
+ * @author Roman Yakimkin
+ * @version 1.0
+ * @since 17.02.2020
+ */
+public class TreeSort {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new TreeSet<>();
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(3);
+        System.out.println(numbers);
+    }
+}
