e4e5af88d99cea4185827710f004692514ab6bf8
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-18 10:29:35 +0300

    Шаблон компаратора + класс для тестирования добавлены

---
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+/**
+ * Класс - компаратор для строк
+ * @author Roman Yakimkin (r.yakimkin@yandex.ru)
+ * @since 18.02.2020
+ * @version 1.0
+ */
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        return 0;
+    }
+}
