099068238cb8cb2312ed323e8eeb09f6bfc718cb
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-20 10:17:29 +0300

    Сортировка User с использованием Comparator[#219866]

--- chapter_003/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/UserTest.java
@@ -2,12 +2,14 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.OrderingComparison.lessThan;
 import static org.junit.Assert.assertThat;
 
 public class UserTest {
@@ -26,11 +28,71 @@ public class UserTest {
     }
 
     @Test
-    public void whenComparePertVSIvan() {
+    public void whenComparePetrVSIvan() {
         int rsl = new User("Petr", 32)
                 .compareTo(
                         new User("Ivan", 31)
                 );
         assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndAge() {
+        Comparator<User> cmpNameAge = new UserDescByName().thenComparing(new UserDescByAge());
+        int rsl = cmpNameAge.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAgeAndName() {
+        Comparator<User> cmp = new UserDescByAge().thenComparing(new UserDescByName());
+        int rsl = cmp.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByNameAndAgeAsc() {
+        Comparator<User> cmpNameAge = new UserAscByName().thenComparing(new UserAscByAge());
+        int rsl = cmpNameAge.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAgeAndNameAsc() {
+        Comparator<User> cmp = new UserAscByAge().thenComparing(new UserAscByName());
+        int rsl = cmp.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByName() {
+        Comparator<User> cmp = new UserAscByName();
+        int rsl = cmp.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenComparatorByAge() {
+        Comparator<User> cmp = new UserAscByAge();
+        int rsl = cmp.compare(
+                new User("Sidorov", 30),
+                new User("Ivanov", 40)
+        );
+        assertThat(rsl, lessThan(0));
     }
 }
