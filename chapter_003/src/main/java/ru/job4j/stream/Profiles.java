36e0151552d473d4520d5564f30596d01a9fdaa5
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-02-28 16:57:51 +0300

    Уникальность элементов и сортировка.[#219751] - сортировка перед удалением дубликатов

--- chapter_003/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_003/src/main/java/ru/job4j/stream/Profiles.java
@@ -20,8 +20,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .distinct()
                 .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
