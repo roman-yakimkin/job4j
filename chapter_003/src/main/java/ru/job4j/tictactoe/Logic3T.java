c3aa7de4829793a184d9f67a66fa4f7a0e403eb4
Roman Yakimkin <r.yakimkin@yandex.ru>
2020-03-12 14:20:05 +0300

    Крестики-нолики на JavaFX[#219760]

--- chapter_003/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_003/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,6 +1,8 @@
 package ru.job4j.tictactoe;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Logic3T {
     private final Figure3T[][] table;
@@ -23,21 +25,31 @@ public class Logic3T {
         return result;
     }
 
+    public boolean isWinner(Predicate<Figure3T> predicate) {
+        return this.fillBy(predicate, 0, 0, 1, 0)
+                || this.fillBy(predicate, 0, 1, 1, 0)
+                || this.fillBy(predicate, 0, 2, 1, 0)
+
+                || this.fillBy(predicate, 0, 0, 0, 1)
+                || this.fillBy(predicate, 1, 0, 0, 1)
+                || this.fillBy(predicate, 2, 0, 0, 1)
+
+                || this.fillBy(predicate, 0, 0, 1, 1)
+                || this.fillBy(predicate, this.table.length - 1, 0, -1, 1);
+    }
+
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
+        return isWinner(Figure3T::hasMarkX);
     }
 
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+        return isWinner(Figure3T::hasMarkO);
     }
 
     public boolean hasGap() {
-        return true;
+        return Arrays.stream(this.table)
+                .flatMap(Arrays::stream)
+                .map(cell -> (cell.hasMarkX() || cell.hasMarkO()))
+                .reduce(false, (acc, el) -> (acc || el));
     }
 }
